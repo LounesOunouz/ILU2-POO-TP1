0b3736205f0995cbcd6ee0a363c7dddfa0989382
Lounes Ounouz <lounesounouz@gmail.com>
2026-03-18 01:23:32 +0100

    Fin TP1

--- src/histoire/ScenarioCasDegrade.java
+++ src/histoire/ScenarioCasDegrade.java
@@ -1,17 +1,23 @@
 package histoire;
 
+import personnages.Gaulois;
 import villagegaulois.Etal;
 
 public class ScenarioCasDegrade {
 	public static void main(String[] args) {
 		Etal etal = new Etal();
+		Gaulois g = new Gaulois("Obélix", 5);
+
+//		Etal etal = new Etal();
+//		Gaulois g = new Gaulois("Obélix", 5);
+
 		try {
-			etal.libererEtal();
+			String resultat = etal.acheterProduit(5, g); // étal vide
+			System.out.println(resultat);
 		} catch (IllegalStateException e) {
-			System.out.println("Gestion d'erreur faite ! ");
+			System.out.println("IllegalStateException capturée => " + e.getMessage());
 		}
-		System.out.println(etal.acheterProduit(5, null));
+
 		System.out.println("Fin du test");
 	}
 }
-//reste b+C +2+village 
